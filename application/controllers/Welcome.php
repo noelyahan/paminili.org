d8570f3116aa1e24888547f7edeb849844c48250
khsushan <khsushan@gmail.com>
2015-09-20 12:53:18 +0530

    add index.php as welcome page

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,7 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->loadHighestRapeComplains();
+		$this->load->view('index.php');
 	}
 
 	function loadBestRatedPoliceStations(){
