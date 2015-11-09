db0458badca0e362fe913d8456808e4b47b27b7b
khsushan <khsushan@gmail.com>
2015-11-10 02:44:53 +0530

    adding add_post,add_comment and add_reply methods

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -5,17 +5,32 @@ class User extends CI_Controller {
     
     
     function add_post() {
-//        $user_id, $description
+        $this->load->model('post_m');
+        $post_m = new Post_m();
+//        $post = $this->input->get('post');
+//        $user_id = $this->input->get('user_id');
+        $post_m->add_post(2,"This is my test post");
     }
     
     
     function add_comment() {
-//        $user_id, $post_id, $description
+       $user_id = $this->input->post('user_id');
+       $post_id = $this->input->post('post_id');
+       $comment_description = $this->input->post('post_description');
+       
+       $this->load->model("Comment_m");
+       $comment = new Comment_m();
+       $comment->add_comment($user_id, $post_id, $comment_description);
     }
     
     
     function add_reply() {
-//        $user_id, $comment_id, $description
+        $this->load->model('reply_m');
+        $reply_m = new Reply_m();
+//        $description = $this->input->get('reply_description');
+//        $user_id = $this->input->get('user_id');
+//        $comment_id = $this->input->get('comment_id');  
+        $reply_m->add_reply(2,2,"This is my test reply comment");
     }
     
     
