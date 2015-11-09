d15eb8c7a108c13a36557c18312098214c462e41
noelyahan <noel.yahan@hotmail.com>
2015-11-10 02:44:26 +0530

    add comment_m add_comment impl

--- application/models/comment_m.php
+++ application/models/comment_m.php
@@ -5,5 +5,24 @@ class Comment_m extends MY_Model {
     const DB_TABLE = 'comment';
     const DB_TABLE_PK = 'comment_id';
     
-    function add_comment($user_id, $post_id, $comment_description) {}
+    public $comment_id;
+    public $description;
+    public $vote_up = 0;
+    public $vote_down = 0;
+    public $timestamp;	
+    public $post_id;
+    public $user_id;
+    
+    function add_comment($user_id, $post_id, $comment_description) {
+        $this->user_id = $user_id;
+        $this->post_id = $post_id;
+        $this->description = $comment_description;
+        $this->timestamp = date('Y-m-d H:i:s');
+        $this->save();
+    }
+    
+    function view_all_comments() {
+        $data = $this->get();
+        var_dump($data);
+    }
 }
