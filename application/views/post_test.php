da2324eb0551ee08c5071e1b89a4a4e631114d73
khsushan <khsushan@gmail.com>
2015-11-12 08:03:57 +0530

    adding post_test  page with div issue

--- application/views/post_test.php
+++ application/views/post_test.php
@@ -9,7 +9,7 @@
         
         <link href="<?php echo base_url(); ?>assets/css/bootstrap.css" rel="stylesheet">
         <link href="<?php echo base_url(); ?>assets/css/font-awesome.css" rel="stylesheet">
-        <link href="<?php echo base_url(); ?>assets/js/morris/morris-0.4.3.min.css" rel="stylesheet">
+<!--        <link href="<?php echo base_url(); ?>assets/js/morris/morris-0.4.3.min.css" rel="stylesheet">-->
         <link href="<?php echo base_url(); ?>assets/css/custom-styles.css" rel="stylesheet">
             
             <!-- Google Fonts-->
@@ -73,7 +73,7 @@
                 <div class="panel panel-primary no-boder bg-color-blue panel-large-margin">
                     <div class="panel-body">
                         <h3 class="page-header">
-                            <img src="assets/img/update_status_icon.png"/>
+                            <img src="<?php echo base_url(); ?>assets/img/update_status_icon.png"/>
                             Update Status
                         </h3>
                         <textArea class="form-control"></textArea></br>
@@ -82,39 +82,45 @@
                         </div>
                     </div>
                 </div>
-
+                
                 <div class="panel panel-primary no-boder">
+                    <?php for($i=0;$i<count($all_posts);$i++){ ?>
                     <div class="panel-body">
+                        
                         <h4 class="page-header bg-color-blue bold">
-                            <img src="assets/img/user_icon.png"/>
-                            Ershadi Sayuri Jayatilaka
-                            <small>1 hour ago</small>
+                            <img src="<?php echo base_url(); ?>assets/img/user_icon.png"/>
+                            <?php echo $all_posts[$i]["user_id"] ?>
+                            <small><?php echo $all_posts[$i]["timestamp"] ?></small>
                         </h4>
                         <div class="page-header">
-                            Behalf of Batch Reps,
-                            Please answer this poll regarding the rescheduling of the Formal Specs, tutorial from
-                            Wednesday to Friday along with the part timers from 7 on wards right after the lecture.
-                            Saman sir wanted a head count regarding this matter ASAP.
-                            Thank You.
+                            <?php echo $all_posts[$i]["post"] ?>
                         </div>
                         <div class="text-left">
-                            <div class="bg-color-blue col-md-2"><h6><img src="assets/img/like_icon.png"/>Like</h6>
+                            <div class="bg-color-blue col-md-2"><h6><img src="<?php echo base_url(); ?>assets/img/like_icon.png"/>Like 
+                            <?php echo $all_posts[$i]["vote_up"] ?>
+                            </h6>
                             </div>
-                            <div class="col-md-2"><h6><img src="assets/img/dislike_icon.png"/>Dislike</h6></div>
-                            <div class="col-md-5"><h6><img src="assets/img/comment_icon.png"/>Comment</h6></div>
+                            <div class="col-md-2"><h6><img src="<?php echo base_url(); ?>assets/img/dislike_icon.png"/>Dislike
+                            <?php echo $all_posts[$i]["vote_down"] ?>    
+                            </h6></div>
+                            <div class="col-md-5"><h6><img src="<?php echo base_url(); ?>assets/img/comment_icon.png"/>Comment</h6></div>
                         </div>
+                        <?php $comments = $all_posts[$i]["comments"] ;
+                               for($j=0;$j<count($comments);$j++){
+                            ?>
                         <div class="col-md-12 ashbox panel">
                             <!--posted comments-->
+                            
                             <div class="col-md-12 panel">
-                                <div class="col-md-1"><img src="assets/img/comment_user_icon.png"/></div>
-                                <h5 class="bg-color-blue bold">Noel Yahan
-                                    <small>2 hours ago</small>
+                                <div class="col-md-1"><img src="<?php echo base_url(); ?>assets/img/comment_user_icon.png"/></div>
+                                <h5 class="bg-color-blue bold"><?php echo $comments[$j]["user_id"] ?>
+                                    <small><?php echo $comments[$j]["timestamp"] ?></small>
                                 </h5>
-                                <div class="col-md-12 font-black panel">Good work Machan</div>
+                                <div class="col-md-12 font-black panel"><?php echo $comments[$j]["description"] ?></div>
                                 <div class="col-md-12 text-left ashbox panel panel-small">
                                     <h6>
-                                        <div class="col-md-1">Like</div>
-                                        <div class="col-md-1">Dislike</div>
+                                        <div class="col-md-1">Like <?php echo $comments[$j]["vote_up"] ?></div>
+                                        <div class="col-md-1">Dislike <?php echo $comments[$j]["vote_down"] ?></div>
                                         <div class="col-md-1"><a data-toggle="collapse" data-parent="#accordion"
                                                                  href="#reply-box" class="">Reply</a></div>
                                     </h6>
@@ -122,49 +128,60 @@
                             </div>
 
                             <!--user reply-->
+                            <?php $replies = $comments[$j]["replies"] ;
+                               for($k=0; $k<count($replies); $k++){     
+                            ?>
                             <div class="col-md-2">
                             </div>
+                            
                             <div class="col-md-10 panel">
-                                <div class="col-md-1"><img src="assets/img/comment_user_icon.png"/></div>
-                                <h5 class="bg-color-blue bold">Nilupul Liyanage
-                                    <small>2 hours ago</small>
+                                <div class="col-md-1"><img src="<?php echo base_url(); ?>assets/img/comment_user_icon.png"/></div>
+                                <h5 class="bg-color-blue bold">  <?php echo $replies[$k]["user_id"] ?>
+                                    <small><?php echo $replies[$k]["timestamp"] ?></small>
                                 </h5>
-                                <div class="col-md-10 font-black panel">Good work Machan</div>
+                                <div class="col-md-10 font-black panel"><?php echo $replies[$k]["description"] ?></div>
                                 <div class="col-md-10 text-left ashbox panel panel-small">
                                     <h6>
-                                        <div class="col-md-1">Like</div>
-                                        <div class="col-md-2">Dislike</div>
+                                        <div class="col-md-1">Like  <?php echo $replies[$k]["vote_up"] ?></div>
+                                        <div class="col-md-2">Dislike <?php echo $replies[$k]["vote_down"] ?></div>
                                         <div class="col-md-1"><a data-toggle="collapse" data-parent="#accordion"
                                                                  href="#reply-box" class="">Reply</a></div>
                                     </h6>
                                 </div>
                             </div>
                             <!--end user reply-->
-
+                                 
                             <!--reply to a user comment-->
+                             <?php } ?>
                             <div class="col-md-2">
                             </div>
+                           
+                            <!-- can't understand--> 
                             <div id="reply-box" class="col-md-10 panel collapse">
                                 <div class="col-md-12">
-                                    <div class="col-md-1"><img src="assets/img/comment_user_icon.png"/></div>
-                                    <h5 class="bg-color-blue bold col-md-7">Sachith Ushan</h5>
-
+                                    <div class="col-md-1"><img src="<?php echo base_url(); ?>assets/img/comment_user_icon.png"/></div>
+                                    <h5 class="bg-color-blue bold col-md-7"><?php echo $user_name ?></h5>
+                                       
                                     <div class="col-md-12 font-black">
                                         <div class="input-group">
                                             <input type="text" class="form-control"
                                                    placeholder="Write a reply...">
                                                     <span class="input-group-btn">
-                                                        <img class="btn btn-default" src="assets/img/enter_icon.png"/>Go
+                                                        <img class="btn btn-default" src="<?php echo base_url(); ?>assets/img/enter_icon.png"/>Go
                                                     </span>
                                         </div>
+                                          
+                                         
                                         <!--user comments-->
                                         <div class="col-md-12 panel">
                                             <div class="col-md-12">
                                                 <div class="col-md-1"><img src="<?php echo base_url(); ?>assets/img/comment_user_icon.png"/></div>
-                                                <h5 class="bg-color-blue bold col-md-10">Sachith Ushan</h5>
+                                                <h5 class="bg-color-blue bold col-md-10"><?php echo $user_name ?></h5>
                                                 <div class="col-md-12 font-black">
                                                     <div class="input-group">
           <input type="text" class="form-control" placeholder="Write a comment...">
+            
+                   
           <span class="input-group-btn">
               <img class="btn btn-default" src="<?php echo base_url(); ?>assets/img/enter_icon.png"/>Go
           </span>
@@ -176,10 +193,11 @@
                                 </div>
                             </div>
                         </div>
+                        <?php } ?>
                         <div class="col-md-3">
                             <div class="panel panel-primary text-center no-boder bg-color-blue">
                                 <div class="panel-body">
-                                    <img src="assets/img/add1.PNG"/>
+                                    <img src="<?php echo base_url(); ?>assets/img/add1.PNG"/>
                                     
                                 </div> 
                             </div>
@@ -187,12 +205,13 @@
  <div class="col-md-3">
                 <div class="panel panel-primary text-center no-boder bg-color-blue">
                     <div class="panel-body">
-                        <img src="assets/img/add2.PNG"/>
-                        <img src="assets/img/add3.PNG"/>
+                        <img src="<?php echo base_url(); ?>assets/img/add2.PNG"/>
+                        <img src="<?php echo base_url(); ?>assets/img/add3.PNG"/>
                     </div>
                 </div>
             </div>
                 </div>
+             <?php  } ?>        
                 <!-- /. PAGE INNER  -->
             </div>
             <!-- /. PAGE WRAPPER  -->
@@ -206,9 +225,9 @@
         <!-- Metis Menu Js -->
         <script src="<?php echo base_url(); ?>assets/js/jquery.metisMenu.js"></script>
         <!-- Morris Chart Js -->
-        <script src="<?php echo base_url(); ?>assets/js/morris/raphael-2.1.0.min.js"></script>
-        <script src="<?php echo base_url(); ?>assets/js/morris/morris.js"></script>
-        <!-- Custom Js -->
-        <script src="<?php echo base_url(); ?>assets/js/custom-scripts.js"></script>
+<!--        <script src="<?php echo base_url(); ?>assets/js/morris/raphael-2.1.0.min.js"></script>-->
+<!--        <script src="<?php echo base_url(); ?>assets/js/morris/morris.js"></script>
+         Custom Js 
+        <script src="<?php echo base_url(); ?>assets/js/custom-scripts.js"></script>-->
     </body>
 </html>
